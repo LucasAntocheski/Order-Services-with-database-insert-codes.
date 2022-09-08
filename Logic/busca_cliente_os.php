626756b826e6a30d344385654a0da007a2fe2f75
LucasAntocheski <108685793+LucasAntocheski@users.noreply.github.com>
2022-09-07 23:57:25 -0300

    Update busca_cliente_os.php

--- Logic/busca_cliente_os.php
+++ Logic/busca_cliente_os.php
@@ -6,7 +6,7 @@
     if(isset($_POST['busca_cliente'])){
        
 
-        $nome = $mysqli -> escape_string( $_POST['busca_cliente']);
+        $nome = $_POST['busca_cliente'];
         $code_sql = "SELECT nome_cliente, id FROM cliente WHERE nome_cliente LIKE '$nome'";
         $consultasituacao = "SELECT situacao FROM cliente WHERE nome_cliente LIKE '$nome';";
         $exec_code = $mysqli -> query($code_sql) or $mysqli -> error;
